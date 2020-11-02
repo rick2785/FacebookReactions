e44fc9fd4e4aab8f1cd4d5e8c4f44635f5b98fe4
rick2785 <rjhrabowskie@gmail.com>
2020-11-02 17:55:54 -0600

    Initial Commit

---
+++ FacebookReactions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookReactions
+//
+//  Created by RJ Hrabowskie on 11/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacebookReactions/FacebookReactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookReactionsApp.swift
+//  FacebookReactions
+//
+//  Created by RJ Hrabowskie on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct FacebookReactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
